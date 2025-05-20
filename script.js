3a390d9429d38b8c5394550e5660536d64d3f49d
Amina <minassouane@gmail.com>
2025-05-20 11:46:50 +0100

    Add function for creating and hovering the dom

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const container = document.querySelector(".container");
 let size = 16;
 
-for (let i = 0; i < size; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  let sizeSquare = 640 / size;
+function makeGrid() {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    let sizeSquare = 640 / size;
 
-  for (let j = 0; j < size; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = `${sizeSquare}px`;
-    square.style.height = `${sizeSquare}px`;
-    row.appendChild(square);
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = `${sizeSquare}px`;
+      square.style.height = `${sizeSquare}px`;
+      row.appendChild(square);
+    }
+
+    container.appendChild(row);
   }
-
-  container.appendChild(row);
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "black";
+function addHover() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
   });
-});
+}
 
 // RESET
 
@@ -30,5 +34,13 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
   size = prompt("What is the number of squares per side for the new grid?");
-  squares.forEach((square) => square.remove());
+  container.replaceChildren();
+
+  if (size <= 100) {
+  } else alert("The maximum number of squares per side is 100!");
+  makeGrid();
+  addHover();
 });
+
+makeGrid();
+addHover();
